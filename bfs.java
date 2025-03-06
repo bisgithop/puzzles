120f9b8c5e96be77d9a3b87f2b2eaac3f18086e1
bisgithop <139035386+bisgithop@users.noreply.github.com>
2025-03-06 23:15:34 +0530

    Create bfs.java

---
+++ bfs.java
@@ -0,0 +1,7 @@
+//BFS - Breadth first search - shortest path - uses queue data structure
+// solving Mazes and Sudoku
+// DAG ( directed acyclic graph) 
+// shortest path and detcting cycles 
+// checkout more details at - https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+
+

81e1c54e1cefce700b5dc8b27399090d8913468c
bisgithop <139035386+bisgithop@users.noreply.github.com>
2025-03-06 22:59:54 +0530

    Update TestStreams.java
    Added questions

--- TestStreams.java
+++ TestStreams.java
@@ -1,10 +1,40 @@
+// Execute the below code here - https://www.w3schools.com/java/tryjava.asp?filename=demo_compiler
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public static void main ( String[] args) {
-  System.out.println("Test");
-}
+public class Main {
+  public static void main(String[] args) {      
+        List<List<String>> listOLists = Arrays.asList(
+            Arrays.asList("Reflection", "Collection", "Stream"),
+            Arrays.asList("Structure", "State", "Flow", "Slippery"),
+            Arrays.asList("Sorting", "Affair", "State", "of", "Affair", "Mapping", "Reduce", "Stream")
+        );
+      
+        Set<String> intermediateResults = new HashSet<>();
+     
+        List<String> result = listOLists.stream()
+            .flatMap(List::stream)               //  can we use lamda expression here ? 
+            .filter(s -> s.startsWith("S"))      // String::startsWith too works here             
+            //.filter(String::startsWith("S"))    // this doesn't work ? 
+            .map(String::toUpperCase)             // String::toUperCase too works here 
+            //.map(s -> s.toUpperCase())          // s -> s.toUpperCase() works  
+            .distinct()                          // Remove duplicate elements
+            .sorted()                            // Sort elements
+            .peek(s -> intermediateResults.add(s)) // Perform an action (add to set) on each element
+            .collect(Collectors.toList());       // Collect the final result into a list
 
+        // Print the intermediate results
+        System.out.println("Intermediate:");
+        intermediateResults.forEach(System.out::println);
+
+        // Print the final result
+        System.out.println("Result:");
+        result.forEach(System.out::println);
+    
+    
+  }
+}
